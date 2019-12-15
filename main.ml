0e4726a3de0526262d3853b822f0d319c886ffa7
Vincent Cordobes <vincent.cordobes@gmail.com>
2019-12-15 12:33:33 +0100

    Init cli

--- main.ml
+++ main.ml
@@ -1,14 +1,14 @@
 open Base
 open Core
 open Stdio
-
+open Cmdliner
 
 (* scheduled review *)
 (* newly introduced and difficult flashcards are shown more frequently while older or less diffcult are shown less frequently *)
 module FlashCard = struct
   type t = {
-    id: string;
-    content: string;
+    name: string;
+    file: string;
   } [@@deriving show, yojson]
 end
 
@@ -48,39 +48,79 @@ module Box = struct
     {frequency; flashcards}
 
   let add flashcard box = 
-    { box with flashcards = flashcard::box.flashcards }
+    {box with flashcards = flashcard :: box.flashcards}
 
   let remove flashcard_id box =
-    { box with 
-      flashcards = List.filter ~f:(fun f -> 
-          not (phys_equal f.id flashcard_id)) box.flashcards }
+    {
+      box with
+      flashcards =
+        List.filter box.flashcards ~f:(fun f ->
+            not String.(f.name = flashcard_id));
+    }
+
 end
 
 
-module Boxes = struct
-  type t = {boxes: Box.t list} [@@deriving show, yojson]
+module Spaced_repetition = struct
+  type t = {boxes: Box.t list} 
+  [@@deriving show, yojson]
+
+  let add flashcard sp =
+    match sp.boxes with
+    | [] -> failwith "Please add a box first"
+    | box :: boxes -> {boxes = Box.add flashcard box :: boxes}
+
+  let load () = 
+    let json_value = Yojson.Safe.from_file "db.json" in
+    let boxes = of_yojson json_value in
+    boxes
+
+  let save sp =
+    let boxes_json = to_yojson sp in
+    Yojson.Safe.pretty_to_channel stdout boxes_json;
+    (* Yojson.Safe.to_file "db.json" boxes_json *)
 end
 
 
-let load_boxes () = 
-  let open Yojson.Safe.Util in
-  let json_value = Yojson.Safe.from_file "db.json" in
-  let boxes = Boxes.of_yojson json_value in
-  boxes
+let path_arg =
+  Arg.(
+    info ["p"; "path"] ~docv:"PATH"
+      ~doc:"The path to the file you want to practice on"
+    |> opt (some file) None
+    |> required)
 
-let save_boxes (boxes) =
-  let boxes_json = Boxes.to_yojson boxes in
-  Yojson.Safe.pretty_to_channel stdout boxes_json
-
-
-let _ =
-  let boxes_result = load_boxes () in
-  printf "Loading boxes...\n";
-  match boxes_result with
-  | Ok boxes -> 
-    printf "%s\n\n" (Boxes.show boxes);
-    printf "Saving boxes...\n";
-    save_boxes boxes
-  | Error e -> printf "Error in %s\n" e
+let name_arg =
+  Arg.(
+    info ["n"; "name"] ~docv:"NAME" ~doc:"Name of the flashcard"
+    |> opt (some string) None
+    |> required)
 
 
+let myProg path name = 
+  printf "Here is the path %s %s" path name
+
+
+let cmd = 
+  Term.(const myProg $ path_arg $ name_arg), Term.info "spaced-repetition"
+
+
+let add path name =
+  let db_result = Spaced_repetition.load () in
+  printf "Loading data...\n";
+  match db_result with
+  | Ok db ->
+      let sp : Spaced_repetition.t =
+        {boxes = List.map db.boxes~f:(Box.add {name; file = path})}
+      in
+      (* printf "%s\n\n" (Spaced_repetition.show sp); *)
+      printf "Saving boxes...\n";
+      Spaced_repetition.save sp
+  | Error e -> fprintf stderr "Error in %s\n" e
+
+
+let add_cmd = 
+  Term.(const add $ path_arg $ name_arg), Term.info "add"
+
+let () = 
+  Term.eval_choice cmd [cmd; add_cmd]
+  |> Term.exit 
